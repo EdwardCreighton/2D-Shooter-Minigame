732fb66b93659447844814da25b0acc48428b67c
Edward Creighton <eduard.chekukha@mail.ru>
2024-01-23 18:34:26 +0300

    App class update

--- ShootingRangeMiniGame/Engine/Core/App.cs
+++ ShootingRangeMiniGame/Engine/Core/App.cs
@@ -1,10 +1,59 @@
-﻿namespace ShootingRangeMiniGame.Engine.Core
+﻿using Leopotam.Ecs;
+
+namespace ShootingRangeMiniGame.Engine.Core
 {
 	public sealed partial class App : Form
 	{
+		private System.Windows.Forms.Timer _timer;
+		private EcsWorld _world;
+		private EcsSystems _systemsRoot;
+
 		public App()
 		{
 			CreateWindow();
+			CreateTimer();
+			CreateEcsInfrastructure();
+		}
+
+		protected override void Dispose(bool disposing)
+		{
+			_timer.Dispose();
+			
+			_systemsRoot?.Destroy();
+			_systemsRoot = null;
+			_world?.Destroy();
+			_world = null;
+			
+			base.Dispose(disposing);
+		}
+
+		protected override void OnLoad(EventArgs e)
+		{
+			_timer.Start();
+
+			base.OnLoad(e);
+		}
+
+		private void CreateTimer()
+		{
+			_timer = new();
+			_timer.Interval = 20;
+			_timer.Tick += (_, _) => GameLoopTick();
+		}
+
+		private void CreateEcsInfrastructure()
+		{
+			_world = new EcsWorld();
+
+			_systemsRoot = new EcsSystems(_world);
+			_systemsRoot.Init();
+		}
+
+		private void GameLoopTick()
+		{
+			_systemsRoot.Run();
+
+			Invalidate();
 		}
 	}
 }
--- ShootingRangeMiniGame/Engine/Core/GameWindow.cs
+++ ShootingRangeMiniGame/Engine/Core/GameWindow.cs
@@ -2,11 +2,29 @@
 {
 	public partial class App
 	{
+		public Size ScreenSize { get; private set; }
+		
 		private void CreateWindow()
 		{
+			ScreenSize = new Size(800, 800);
+			
 			AutoScaleMode = AutoScaleMode.Font;
-			ClientSize = new Size(800, 450);
+			ClientSize = ScreenSize;
+			FormBorderStyle = FormBorderStyle.FixedSingle;
+			MaximizeBox = false;
+			MinimizeBox = false;
 			Text = "Shooting Range Mini Game";
+
+			CreateGraphics();
+			
+			DoubleBuffered = true;
+		}
+
+		protected override void OnPaint(PaintEventArgs e)
+		{
+			e.Graphics.Clear(Color.LightBlue);
+			
+			base.OnPaint(e);
 		}
 	}
 }
