3780b45296bde66a36350106541abc157f317383
Edward Creighton <eduard.chekukha@mail.ru>
2024-01-25 05:13:33 +0300

    UserInput.cs bug fix

--- ShootingRangeMiniGame/Engine/Core/UserInput.cs
+++ ShootingRangeMiniGame/Engine/Core/UserInput.cs
@@ -12,8 +12,20 @@ namespace ShootingRangeMiniGame.Engine.Core
 			base.OnMouseMove(e);
 
 			MousePosition = new Vector2(e.Location.X, e.Location.Y);
+		}
+
+		protected override void OnMouseDown(MouseEventArgs e)
+		{
+			base.OnMouseDown(e);
 
 			LeftMouseButtonPressed = e.Button == MouseButtons.Left;
+		}
+
+		protected override void OnMouseUp(MouseEventArgs e)
+		{
+			base.OnMouseUp(e);
+
+			LeftMouseButtonPressed = e.Button != MouseButtons.Left;
 		}
 	}
 }
