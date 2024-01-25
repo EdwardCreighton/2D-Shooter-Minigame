227f7abcc87348729835ca95bd8a4c7b49f7b4f3
Edward Creighton <eduard.chekukha@mail.ru>
2024-01-25 04:45:02 +0300

    Player input refactor

---
+++ ShootingRangeMiniGame/Assets/Systems/PlayerRotationSystem.cs
@@ -0,0 +1,24 @@
+﻿using System.Numerics;
+using Leopotam.Ecs;
+using ShootingRangeMiniGame.Engine;
+using ShootingRangeMiniGame.Engine.Core;
+using ShootingRangeMiniGame.Engine.Components;
+using ShootingRangeMiniGame.Assets.Markers;
+
+namespace ShootingRangeMiniGame.Assets.Systems
+{
+	public class PlayerRotationSystem : IEcsRunSystem
+	{
+		private EcsFilter<PlayerMarker> _filter;
+		private App _app;
+		
+		public void Run()
+		{
+			ref var transform = ref _filter.GetEntity(0).Get<Transform>();
+			
+			Vector2 aimDirection = _app.MousePosition - transform.Position;
+			aimDirection = Vector2.Normalize(aimDirection);
+			transform.Rotation = aimDirection.Angle();
+		}
+	}
+}
--- ShootingRangeMiniGame/Assets/Systems/PlayerShootSystem.cs
+++ ShootingRangeMiniGame/Assets/Systems/PlayerShootSystem.cs
@@ -1,27 +1,25 @@
 ﻿using System.Numerics;
 using Leopotam.Ecs;
-using ShootingRangeMiniGame.Engine;
 using ShootingRangeMiniGame.Engine.Core;
 using ShootingRangeMiniGame.Engine.Components;
 using ShootingRangeMiniGame.Assets.Markers;
 
 namespace ShootingRangeMiniGame.Assets.Systems
 {
-	public class PlayerInputSystem : IEcsRunSystem
+	public class PlayerShootSystem : IEcsRunSystem
 	{
-		private App _app;
 		private EcsFilter<PlayerMarker> _filter;
-
+		private App _app;
+		
 		public void Run()
 		{
-			ref var transform = ref _filter.GetEntity(0).Get<Transform>();
-			
-			Vector2 aimDirection = _app.MousePosition - transform.Position;
-			aimDirection = Vector2.Normalize(aimDirection);
-			transform.Rotation = aimDirection.Angle();
-
 			if (_app.LeftMouseButtonPressed)
 			{
+				ref var transform = ref _filter.GetEntity(0).Get<Transform>();
+			
+				Vector2 aimDirection = _app.MousePosition - transform.Position;
+				aimDirection = Vector2.Normalize(aimDirection);
+				
 				ref var onSpawnProjectile = ref _filter.GetEntity(0).Get<OnSpawnProjectile>();
 				onSpawnProjectile.Position = transform.Position;
 				onSpawnProjectile.Direction = aimDirection;
